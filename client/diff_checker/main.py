dea4632cf12c861117e0120d058b31374cda06f8
Samad <faroos3@rpi.edu>
2017-03-04 12:56:13 -0500

    initial commit to just define directories and a file

---
+++ client/diff_checker/main.py
@@ -0,0 +1,6 @@
+'''
+This is going to be where Samad test things out with text files or whatever.
+Going to see if I can split up files line by line, put them into a list, use a data
+structure, all that jazz. 
+
+'''
