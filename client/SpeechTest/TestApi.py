48e55e4621588a2aac150677bb09a2140d4be33f
SjBeale <beales@rpi.edu>
2017-04-09 19:18:55 -0400

    Added pocketsphinx example, it's an alternative but right now it doesn't seem to work any better or worse

--- client/SpeechTest/TestApi.py
+++ client/SpeechTest/TestApi.py
@@ -7,6 +7,14 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
+# This is an alternative to goolge if we want, but it's also a requirment for the google api
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
 # recognize speech using Google Speech Recognition
 try:
      #right now , we are using the default API key
@@ -17,4 +25,5 @@ except sr.UnknownValueError:
    print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 
